c806c2ccb5ef915726a23ebab934f391951e7af4
ownport <ownport@gmail.com>
2013-11-14 09:20:21 +0200

    move settings to specific file

---
+++ dblite/settings.py
@@ -0,0 +1,8 @@
+# the list of supported backends
+SUPPORTED_BACKENDS = ['sqlite',]
+
+# how many items will be returned by one SQL request. 
+# Used in Storage()._get_all()
+ITEMS_PER_REQUEST = 1000
+
+
