c89bf4e41b98d01a657fa1a2ee185fc5ab45382b
ownport <ownport@gmail.com>
2016-01-15 23:41:43 +0200

    [-] create-env.sh

--- scripts/create-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo 'Creating user: dev ...'
-adduser --disabled-password --gecos '' --uid 1000 dev 
-adduser dev sudo
-echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-chown -R dev:dev /data/scrapy-dblite
-exec su - dev
-
